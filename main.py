cd31ecee6edbdbed5967368b9880625e7a66136c
unknown <sej324@lehigh.edu>
2023-12-26 08:05:40 -0500

    main.py has been created, and search all users in the vocie channels

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    all_members = []  # 모든 멤버들을 저장할 리스트
     print(f'Logged in as {client.user}!')
     for guild in client.guilds:  # 연결된 모든 서버(길드)를 순회
         if guild.name != 'Discord Bot Project':
@@ -17,8 +18,12 @@ async def on_ready():
                 members = channel.members
                 if members:
                     member_list = ', '.join([member.display_name for member in members])
+                    if members:
+                        member_list = [member.display_name for member in members]
+                        all_members.extend(member_list)
                     print(f'Members in {channel.name}: {member_list}')
                 else:
                     print(f'No members in {channel.name}')
-
+    print("All members in voice channels:", all_members)
+    print("Total members:", len(all_members))
 client.run('MTE4ODk1MjE0OTY1MzIwNTAzMg.Grz3Ed.aQ-pI_X8fq9M3mJbZV6zckLEX6lV-c1oegKXxE')
