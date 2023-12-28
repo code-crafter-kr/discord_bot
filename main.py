f4f4ac20dc83c9215ce95e6a5b68d4d7697cf045
Sehyoun_Jang <sej324@lehigh.edu>
2023-12-28 14:30:33 -0500

    client ID has been added, send the file and message both

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 CHANNEL_ID = 1071803555926790177 # server id
 CLIENT_ID = 332468315895365632 # user id (client id)
+CLIENT_ID = 356357918310006794
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -33,25 +34,28 @@ async def on_ready():
     print("All members in voice channels:", all_members)
     print("Total members:", len(all_members))
 
-    # # Save the list of members to a file
-    gd.save_list_to_file(all_members, 'backup', 'members.txt')
+    # # # Save the list of members to a file
+    # gd.save_list_to_file(all_members, 'backup', 'members.txt')
 
-    df = gd.create_hashed_df(all_members)
-    printing_result = gd.create_print_df(df)
+    # df = gd.create_hashed_df(all_members)
+    # printing_result = gd.create_print_df(df)
 
-    # # Send the message to the channel
-    channel = client.get_channel(CHANNEL_ID)    
-    if channel:
-        await channel.send(printing_result)
-    else:
-        print(f"Channel with ID {CHANNEL_ID} not found.")
+    # # # Send the message to the channel
+    # channel = client.get_channel(CHANNEL_ID)    
+    # if channel:
+    #     await channel.send(printing_result)
+    # else:
+    #     print(f"Channel with ID {CHANNEL_ID} not found.")
     
     user = await client.fetch_user(CLIENT_ID)  # get the user object
 
     today_date = datetime.today().strftime('%Y-%m-%d')
-    FILE_PATH = 'backup' + today_date + '/members.csv'
+    FILE_PATH = 'backup/' + today_date + '/members.csv'
+    filename = datetime.today().strftime('%Y-%m-%d') + ".csv"
+    # send message to the user
     with open(FILE_PATH, 'rb') as file:  # open the file
-        await user.send(file=discord.File(file, 'filename.csv'))  # send it to the user
+        await user.send("오늘자 출석입니다.")
+        await user.send(file=discord.File(file, filename))  # send it to the user
     
 
 client.run('MTE4ODk1MjE0OTY1MzIwNTAzMg.Grz3Ed.aQ-pI_X8fq9M3mJbZV6zckLEX6lV-c1oegKXxE')
