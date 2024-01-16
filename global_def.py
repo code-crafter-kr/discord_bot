0de49bfe8afd96066fc07403b2f225a666d660b9
Sehyoun_Jang <sej324@lehigh.edu>
2024-01-15 22:58:03 -0500

    added functions, specific filename, run.txt - run exit function

--- global_def.py
+++ global_def.py
@@ -5,10 +5,64 @@ from datetime import datetime
 from collections import defaultdict
 import pandas as pd 
 
+
+def get_attendance_message(param, today_date):
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    messages = {
+        "화요일공성": "화요일 공성 출석",
+        "수요일공성": "수요일 공성 출석",
+        "목요일공성": "목요일 공성 출석",
+        "금요일공성": "금요일 공성 출석",
+        "평일분쟁2000": "평일 분쟁 오후 8시 출석",
+        "평일분쟁2300": "평일 분쟁 오후 11시 출석",
+        "주말분쟁1430": "주말 분쟁 오후 2시 30분 출석",
+        "주말분쟁1500": "주말 분쟁 오후 3시 00분 출석",
+        "주말분쟁2000": "주말 분쟁 오후 8시 출석",
+        "주말분쟁2300": "주말 분쟁 오후 11시 출석"
+    }
+    
+    return messages.get(param, f"{today_date} : {param} 입니다")
+
+def get_attendance_filemname_txt(param, today_date):
+    messages = {
+        "화요일공성": f"{today_date}_화요일공성.txt",
+        "수요일공성": f"{today_date}_수요일공성.txt",
+        "목요일공성": f"{today_date}_목요일공성.txt",
+        "금요일공성": f"{today_date}_금요일공성.txt",
+        "평일분쟁2000": f"{today_date}_평일분쟁2000.txt",
+        "평일분쟁2300": f"{today_date}_평일분쟁2300.txt",
+        "주말분쟁1430": f"{today_date}_주말분쟁1430.txt",
+        "주말분쟁1500": f"{today_date}_주말분쟁1500.txt",
+        "주말분쟁2000": f"{today_date}_주말분쟁2000.txt",
+        "주말분쟁2300": f"{today_date}_주말분쟁2300.txt"
+    }
+    
+    return messages.get(param, f"{today_date}_{param}.txt")
+
+def get_attendance_filemname_csv(param, today_date):
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    messages = {
+        "화요일공성": f"{today_date}_화요일공성.csv",
+        "수요일공성": f"{today_date}_수요일공성.csv",
+        "목요일공성": f"{today_date}_목요일공성.csv",
+        "금요일공성": f"{today_date}_금요일공성.csv",
+        "평일분쟁2000": f"{today_date}_평일분쟁2000.csv",
+        "평일분쟁2300": f"{today_date}_평일분쟁2300.csv",
+        "주말분쟁1430": f"{today_date}_주말분쟁1430.csv",
+        "주말분쟁1500": f"{today_date}_주말분쟁1500.csv",
+        "주말분쟁2000": f"{today_date}_주말분쟁2000.csv",
+        "주말분쟁2300": f"{today_date}_주말분쟁2300.csv"
+    }
+    
+    return messages.get(param, f"{today_date}_{param}.csv")
+
+
+
 # Function to save a list to a file in a directory named with today's date
-def save_list_to_file(list_to_save, base_directory, file_name):
-    # Get today's date in the format YYYYMMDD
-    today_date = datetime.now().strftime("%Y-%m-%d")
+def save_list_to_file(list_to_save, base_directory, param, today_date):
+    
+    file_txt = get_attendance_filemname_txt(param, today_date)
+    file_csv = get_attendance_filemname_csv(param, today_date)
 
     # Create a new directory with today's date
     date_directory = f"{base_directory}/{today_date}"
@@ -16,7 +70,7 @@ def save_list_to_file(list_to_save, base_directory, file_name):
         os.makedirs(date_directory)
 
     # Full path for the file
-    full_file_path = f"{date_directory}/{file_name}"
+    full_file_path = f"{date_directory}/{file_txt}"
 
     # Convert the list to string including brackets
     list_as_string = str(list_to_save)
@@ -27,7 +81,7 @@ def save_list_to_file(list_to_save, base_directory, file_name):
 
     df = create_hashed_df(list_to_save)
     # save df
-    df.to_csv(f"{date_directory}/members.csv", index=False, encoding='utf-8-sig')
+    df.to_csv(f"{date_directory}/{file_csv}", index=False, encoding='utf-8-sig')
 
 # Function to save a list to a file in a directory named with today's date
 def get_before_at(text):
@@ -92,8 +146,12 @@ def create_hashed_df(lst):
 
     return df  # 데이터가 일치하면 DataFrame 반환
 
-def create_print_df(df):
-    result = ""
+def create_print_df(df, param):
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    today_type = get_attendance_message(param, today_date)
+
+    result = f"{today_date} : {today_type} 입니다\n"
+
     total_non_none_count = df.notnull().sum().sum()
     for column in df:
         non_null_rows = df[column].dropna()
@@ -103,9 +161,3 @@ def create_print_df(df):
         result += "-" + joined_rows + '\n\n'
     result += f"총 인원: {total_non_none_count}\n"
     return result
-
-
-# Example usage
-if __name__ == "__main__":
-    lst =  ['死1@반함', '인연@수안', '유리@져햐', '유리@마강마도', '유리@머머', '의지@미백', '유리@대초니', '약속2@요거', '빛@해 리포터', '로망@농협', '시크릿@초보', '빛@광안리가자', '유리@찐따같은초보', '로망@제압 X', '유리@반격', '의리2@강도차승준', '유리@유천우', '유리@라자 바', '약속4@김가율(X)', '의지@뫼뇌 x', '달빛@패브', '약속6@황썬우', '의리2@우드워드', '빛@무현', '의리2@보리', '약속6@극용랑', '약속3@제일궁다샙', ' 死4@보덕국왕 X', '死@따구미 X', '死3@쥐센세', '로망@문안나', '유리@조꺄치', '달빛@미나', '死5@도도코', '死1@습관', '의지@냥이', '약속1@배출(X)', '死@김쭈닝3 X', '약속4@의뀨(X)', '약속3@하땀', '死5@배구', '인연@순각', '로망@분유', '死3@세겹', '약속2@민채', '약속@제빔', '死1@피치', '로망@쿠팡윙X', '유리2@솔랭', '死3@뚠서3 X', '약속1@예리 X', '로망@귀문킬러', '약속1@나나토', '의리1@에스급', '빛@이브x', '인연@잠양', '극한@니트언니', '死1@달래1', '약속6@노래활', '死3@주니', '로망@갓방포', '빛@레리x', '극한@원거리초보', '약속4@포맨(x)', '死1@달링', '의리1@매쫑', '빛@칸포', '死1@초급', '로망@휴지심', '의지@순궁', '의리2@듀님', '약속@비책(듣기용)', '의지@헤이', '빛@으둥', '死1@어멍네', '약속1@어제', '死3@소금3', '의리1@성용몬', '빛@설라X', '약속3@급박', '유리@방어해제', '약속1@상담', '유리@이유나', '빛@섹션', '유리@첼채', '인연@니유', '유리@클링', '약속3@로빈달래', '유리@식객', '약속4@타라수', '로망@원닢', '死1@하히', '시크릿@단요한', '死4@해찬갓4', '유리@나비', '死3@풋볼2 X', '빛@소울트리', '로망@휴디', '달빛@강가', '死2@화니버프(듣기)', '시크릿@호두맥주', '약속3@섬연', '유리@도둑김용수', '死5@후지토라', '약속3@러브힐', '약속4@노숙', '약속@시하', '의리1@파트하', '死4@도톨임', '시 크릿@천치', '인연@재원', '약속2@호로', '약속6@킴럼마', '약속3@팍쿠니', '로망@갈남순', '의리1@힐타치', '인연@슬랜드', '빛@어도', '시크릿@연탈토', '달 빛@지하', '약속2@아가호랑', '로망@재요', '달빛@꿀꺽', '死1@사기', '로망@허효찌', '死5@구탁', '달빛@밤밤', '로망@카디타', '약속2@벨로치', '死5@아이', '달빛@불만', '死3@김삐냥', '빛@극압', '극한@사냥개고수', '유리@사수이광민', '死5@김숙', '로망@홀릭X', '달빛@꼬꼬닦', '약속1@대학생', '약속1@우웅(x)', '약속2@화룡비보', '시크릿@꽃희', '유리@우웽', '로망@투시아', '유리@참젖', '달빛@고천득', '빛@카드', '의리1@나인', '의지@윤인진', 'Developer', '유리1@제코', '약속1@유럽', '死3@신검', '의지@쁠레기X', '달빛@동화', '약속2@호랑', '유리@방역소', '약속6@이수', '약속@신궁', '유리@사숱', '약속4@오프벨', '유리@보이면삼초캇', '로망@배터지는콜라X', '의리1@방심', '달빛@이경진', '달빛@미올', '빛@둘레길', '빛@별새우', '유리@알바코어X', '의지@자몽리', '死4@ 미여누 X', '死3@렌쥬', '의지@넋니', '유리@강지영', '유리@빙구업', '00약속@비책', '약속3@이디은', '약속3@아가도하', '로망@탐키x', '약속2@로우', '유리@종교전쟁X', '로망@싫어', '시크릿@락두', '死5@서포터니까', '약속2@네이x', '로망@앗쉐', '로망@바니', '빛@파도가머물던', '시크릿@응왜', '약속2@군주', ' 의리2@영준', '로망@사신리퍼X', '달빛@빙글', '로망@애틋', '의지@돔황쳐X', '약속1@석윤', '死4@치타X', '死2@화니버프2 ●', '死2@행적2', '死2@후시딘2', ' 死2@꽃서비2', '死2@탐욕의이빨', '死2@탄뎅2', '死2@마도문어2', '死2@자신2']
-    save_list_to_file(lst, 'backup', 'members.txt')
--- main.py
+++ main.py
@@ -1,61 +1,82 @@
 import discord
+import sys
 import global_def as gd
 from datetime import datetime
 
-
-CHANNEL_ID = 1071803555926790177 # server id
-CLIENT_ID = 332468315895365632 # user id (client id)
+CHANNEL_ID = 1071803555926790177
+CLIENT_ID = 332468315895365632 # 둘레님
+# CLIENT_ID = 356357918310006794 # 나
 
 intents = discord.Intents.default()
 intents.guilds = True
-intents.members = True  # Subscribe to the privileged members intent.
+intents.members = True
 client = discord.Client(intents=intents)
+
+
+
+def gather_voice_channel_members(client): #완성형
+    all_members = []
+    for guild in client.guilds:
+        if guild.name != 'Discord Bot Project':
+            for channel in guild.voice_channels:
+                members = channel.members
+                if members:
+                    all_members.extend([member.display_name for member in members])
+    return all_members #모든 멤버 리스트로 반환환
+
+async def save_and_print_set(members, param):
+    gd.save_list_to_file(members, 'backup', param, today_date) # txt 및 csv 파일로 변환
+    df = gd.create_hashed_df(members) # 해시 df로 변환 출력용
+    return gd.create_print_df(df, param) # 출력용 form 으로 변환
+
+
+async def send_channel_message(client, message):
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send(message)
+    else:
+        print(f"Channel with ID {CHANNEL_ID} not found.")
+
+async def send_user_file(client, file_path, filename):
+    user = await client.fetch_user(CLIENT_ID)
+    with open(file_path, 'rb') as file:
+        today_type = gd.get_attendance_message(param, today_date)
+        await user.send(f"{today_type} 입니다.")
+        await user.send(file=discord.File(file, filename))
 
 @client.event
 async def on_ready():
-    all_members = []  # list for all members in voice channels
+    global param, today_date
+    file_name = gd.get_attendance_filemname_csv(param, today_date)
     print(f'Logged in as {client.user}!')
-    for guild in client.guilds:  # traverse all guilds that bot is in
-        if guild.name != 'Discord Bot Project':
-            print(f'Looking in guild: {guild.name}')
-            for channel in guild.voice_channels:  # traverse all voice channels in the guild
-                print(f'Checking voice channel: {channel.name}')
-                members = channel.members
-                if members:
-                    member_list = ', '.join([member.display_name for member in members])
-                    if members:
-                        member_list = [member.display_name for member in members]
-                        all_members.extend(member_list)
-                    print(f'Members in {channel.name}: {member_list}')
-                else:
-                    print(f'No members in {channel.name}')
-
-    print("All members in voice channels:", all_members)
-    print("Total members:", len(all_members))
-
-    # # Save the list of members to a file
-    gd.save_list_to_file(all_members, 'backup', 'members.txt')
-
-    df = gd.create_hashed_df(all_members)
-    printing_result = gd.create_print_df(df)
-
-    # # Send the message to the channel
-    channel = client.get_channel(CHANNEL_ID)    
-    if channel:
-        await channel.send(printing_result)
-    else:
-        print(f"Channel with ID {CHANNEL_ID} not found.")
-    
-    user = await client.fetch_user(CLIENT_ID)  # get the user object
-
+    members = gather_voice_channel_members(client)
+    print("Total members:", len(members))
+    message = await save_and_print_set(members, param)
+    await send_channel_message(client, message)
     today_date = datetime.today().strftime('%Y-%m-%d')
-    FILE_PATH = 'backup/' + today_date + '/members.csv'
-    filename = datetime.today().strftime('%Y-%m-%d') + ".csv"
-    # send message to the user
-    with open(FILE_PATH, 'rb') as file:  # open the file
-        await user.send("오늘자 출석입니다.")
-        await user.send(file=discord.File(file, filename))  # send it to the user
-
+    file_path = f'backup/{today_date}/{file_name}'
+    await send_user_file(client, file_path, file_name)
     await client.close()
+    sys.exit()  # 스크립트 실행을 종료합니다.
 
-client.run('MTE4ODk1MjE0OTY1MzIwNTAzMg.Grz3Ed.aQ-pI_X8fq9M3mJbZV6zckLEX6lV-c1oegKXxE')
+def run_file(param):
+    with open('run.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            if line.strip():  # 빈 줄이 아닌 경우에만 처리
+                key, value = line.split(':')
+                if key.strip() == param and value.strip() == '0':
+                    return False
+    return True
+
+def run_bot(token):
+    client.run(token)
+
+
+
+param = input() #cron 설정에서 넘겨받을 예정
+today_date = datetime.today().strftime('%Y-%m-%d')
+
+if not run_file(param): # run.txt 에서 해당 파라미터가 0이면 종료
+    sys.exit()
+
+run_bot('MTE4ODk1MjE0OTY1MzIwNTAzMg.Grz3Ed.aQ-pI_X8fq9M3mJbZV6zckLEX6lV-c1oegKXxE')
